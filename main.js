a78e3013e286e9181f099f7a995371f659a08435
Ti-mmy552 <nsetimothy985@gmail.com>
2025-09-18 01:17:59 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,9 @@
+console.log('Hello World!');
+function showSideBar(){
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'block';
+}
+function hideSideBar() {
+  const sidebar = document.querySelector('.sidebar');
+  sidebar.style.display = 'none';
+}
